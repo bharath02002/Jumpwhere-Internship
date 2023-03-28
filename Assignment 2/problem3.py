d6e2b6c2e957ed9b12e586e2fa9056f56d613aed
Bharathesh K <102503551+bharath02002@users.noreply.github.com>
2023-03-28 10:21:11 +0530

    Add files via upload

---
+++ Assignment 2/problem3.py
@@ -0,0 +1,13 @@
+# Kth largest element in array 
+
+
+arr = [4,2,9,7,5,6,7,1,3]
+k = 4
+
+def kth_largest(arr, k):
+    n = len(arr)
+    arr.sort()
+    return arr[n-k]
+
+large = kth_largest(arr,k)
+print("The kth largest element is ",large)
